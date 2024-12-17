ffc0d564ee405175032242ca7a523e0849d8aa4c
matusberes43 <matusvanberes@gmail.com>
2024-12-17 17:21:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-// Skript pre jednoduché zobrazenie
-console.log("Portfólio načítané úspešne!");
+const API_KEY = 'VAŠ_API_KĽÚČ';
+const API_URL = 'https://api.themoviedb.org/3';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
-// Potenciálna interaktivita môže byť pridaná neskôr
+document.getElementById('search-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const query = document.getElementById('search-input').value;
+    if (query) {
+        searchMovies(query);
+    }
+});
+
+async function searchMovies(query) {
+    const response = await fetch(`${API_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+    const data = await response.json();
+    displayMovies(data.results);
+}
+
+function displayMovies(movies) {
+    const movieContainer = document.getElementById('movie-container');
+    movieContainer.innerHTML = '';
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+            <img src="${IMAGE_URL + movie.poster_path}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>Hodnotenie: ${movie.vote_average}</p>
+        `;
+        movieContainer.appendChild(movieElement);
+    });
+}
