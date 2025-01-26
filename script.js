fe8cc22bcf4add0dce464d1d3e5c9287dff8af9e
matusberes43 <matusvanberes@gmail.com>
2025-01-26 22:44:01 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+// Funkcia pre zobrazenie konšpiračných teórií USA
+function showUSA() {
+  const contentBox = document.getElementById("content-box");
+  contentBox.innerHTML = `
+    <h2>KONŠPIRAČNÉ TEÓRIE USA</h2>
+    <ul>
+      <li>Area 51 a mimozemšťania</li>
+      <li>11. september 2001 (9/11)</li>
+      <li>Zavraždenie Johna F. Kennedyho</li>
+      <li>Pristátie na Mesiaci</li>
+      <li>Projekt MKUltra</li>
+      <li>Roswellský incident</li>
+      <li>Chemtrails</li>
+      <li>Illumináti a Nový svetový poriadok</li>
+      <li>Smrť Marilyn Monroe</li>
+      <li>Smrť Jeffreyho Epsteina</li>
+      <li>HAARP a manipulácia počasia</li>
+      <li>QAnon</li>
+      <li>Denver International Airport a podzemné základne</li>
+      <li>Paul McCartney je mŕtvy (Beatles teória)</li>
+      <li>COVID-19 ako biologická zbraň alebo podvod</li>
+    </ul>
+  `;
+}
+
+// Funkcia pre zobrazenie konšpiračných teórií sveta
+function showWorld() {
+  const contentBox = document.getElementById("content-box");
+  contentBox.innerHTML = `
+    <h2>KONŠPIRAČNÉ TEÓRIE SVET</h2>
+    <ul>
+      <li>Smrť princeznej Diany (Spojené kráľovstvo)</li>
+      <li>Záhada Dyatlovovej výpravy (Rusko)</li>
+      <li>Zmiznutie letu MH370 (Malajzia)</li>
+      <li>Černobyľská katastrofa (Ukrajina/Sovietsky zväz)</li>
+      <li>Tunguská udalosť (Rusko)</li>
+      <li>Stonehenge (Spojené kráľovstvo)</li>
+      <li>Nacisti v Antarktíde (Nemecko)</li>
+    </ul>
+  `;
+}
