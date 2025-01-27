2ddf3861c75418316727bfb813704a1b85bc0ae7
matusberes43 <matusvanberes@gmail.com>
2025-01-27 19:08:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,73 +6,67 @@ function createBoxes(theories, containerId) {
   theories.forEach((theory) => {
     const box = document.createElement("div");
     box.className = "box";
-    box.innerHTML = `<i class="fas fa-question"></i> ${theory}`; // Pridaná ikona
+    box.innerHTML = `<i class="${theory.icon}"></i> ${theory.name}`; // Pridaná ikona
     contentBox.appendChild(box);
   });
 }
 
+// Zoznam teórií USA s ikonkami
+const usaTheories = [
+  { name: "Area 51 a mimozemšťania", icon: "fas fa-ufo" },
+  { name: "11. september 2001", icon: "fas fa-building" },
+  { name: "Zavraždenie Johna F. Kennedyho", icon: "fas fa-user-secret" },
+  { name: "Pristátie na Mesiaci", icon: "fas fa-moon" },
+  { name: "Projekt MKUltra", icon: "fas fa-brain" },
+  { name: "Roswellský incident", icon: "fas fa-space-shuttle" },
+  { name: "Chemtrails", icon: "fas fa-cloud" },
+  { name: "Illumináti a Nový svetový poriadok", icon: "fas fa-eye" },
+  { name: "Smrť Marilyn Monroe", icon: "fas fa-star" },
+  { name: "Smrť Jeffreyho Epsteina", icon: "fas fa-skull" },
+  { name: "HAARP a manipulácia počasia", icon: "fas fa-bolt" },
+  { name: "QAnon", icon: "fas fa-question" },
+  { name: "Denver International Airport a podzemné základne", icon: "fas fa-plane" },
+  { name: "Paul McCartney je mŕtvy", icon: "fas fa-music" },
+  { name: "COVID-19 ako biologická zbraň alebo podvod", icon: "fas fa-virus" }
+];
+
+// Zoznam teórií sveta s ikonkami
+const worldTheories = [
+  { name: "Smrť princeznej Diany", icon: "fas fa-crown" },
+  { name: "Záhada Dyatlovovej výpravy", icon: "fas fa-mountain" },
+  { name: "Zmiznutie letu MH370", icon: "fas fa-plane" },
+  { name: "Černobyľská katastrofa", icon: "fas fa-radiation" },
+  { name: "Tunguská udalosť", icon: "fas fa-meteor" },
+  { name: "Stonehenge", icon: "fas fa-monument" },
+  { name: "Nacisti v Antarktíde", icon: "fas fa-igloo" }
+];
+
 // Funkcia pre zobrazenie konšpiračných teórií USA
 function showUSA() {
-  const usaTheories = [
-    "Area 51 a mimozemšťania",
-    "11. september 2001",
-    "Zavraždenie Johna F. Kennedyho",
-    "Pristátie na Mesiaci",
-    "Projekt MKUltra",
-    "Roswellský incident",
-    "Chemtrails",
-    "Illumináti a Nový svetový poriadok",
-    "Smrť Marilyn Monroe",
-    "Smrť Jeffreyho Epsteina",
-    "HAARP a manipulácia počasia",
-    "QAnon",
-    "Denver International Airport a podzemné základne",
-    "Paul McCartney je mŕtvy",
-    "COVID-19 ako biologická zbraň alebo podvod"
-  ];
-
   createBoxes(usaTheories, "content-box");
-  playClickSound();
+  playClickSound("click-sound-usa");
 }
 
 // Funkcia pre zobrazenie konšpiračných teórií sveta
 function showWorld() {
-  const worldTheories = [
-    "Smrť princeznej Diany",
-    "Záhada Dyatlovovej výpravy",
-    "Zmiznutie letu MH370",
-    "Černobyľská katastrofa",
-    "Tunguská udalosť",
-    "Stonehenge",
-    "Nacisti v Antarktíde"
-  ];
-
   createBoxes(worldTheories, "content-box");
-  playClickSound();
+  playClickSound("click-sound-world");
 }
 
 // Funkcia pre zobrazenie náhodnej teórie
 function showRandomTheory() {
-  const allTheories = [
-    "Area 51 a mimozemšťania",
-    "11. september 2001",
-    "Zavraždenie Johna F. Kennedyho",
-    "Smrť princeznej Diany",
-    "Záhada Dyatlovovej výpravy",
-    "COVID-19 ako biologická zbraň alebo podvod"
-  ];
-
+  const allTheories = [...usaTheories, ...worldTheories];
   const randomTheory = allTheories[Math.floor(Math.random() * allTheories.length)];
   const contentBox = document.getElementById("content-box");
-  contentBox.innerHTML = `<div class="box"><i class="fas fa-dice"></i> ${randomTheory}</div>`;
-  playClickSound();
+  contentBox.innerHTML = `<div class="box"><i class="${randomTheory.icon}"></i> ${randomTheory.name}</div>`;
+  playClickSound("click-sound-random");
 }
 
 // Funkcia pre resetovanie obsahu
 function resetContent() {
   const contentBox = document.getElementById("content-box");
   contentBox.innerHTML = "";
-  playClickSound();
+  playClickSound("click-sound-reset");
 }
 
 // Funkcia pre prepínanie tmavého režimu
@@ -86,12 +80,12 @@ function toggleDarkMode() {
   } else {
     button.innerHTML = '<span class="icon">🌙</span> Režim tmavého svetla';
   }
-  playClickSound();
+  playClickSound("click-sound-darkmode");
 }
 
 // Funkcia pre prehratie zvukového efektu
-function playClickSound() {
-  const clickSound = document.getElementById("click-sound");
+function playClickSound(soundId) {
+  const clickSound = document.getElementById(soundId);
   if (clickSound) {
     clickSound.play();
   } else {
